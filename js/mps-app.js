b22d1ef31924af3d2f0ef63fa102f407610a343b
vijitha <vijithaepa@gmail.com>
2016-07-19 17:42:28 +1000

    regular updates

--- js/mps-app.js
+++ js/mps-app.js
@@ -2,7 +2,9 @@
  * Created by EpaV on 12/07/2016.
  */
 (function(){
-    var app = angular.module("mps-app", ["ngRoute"]);
+    'use strict';
+
+    var app = angular.module("mps-app", ['ngMaterial','ngRoute']);
 
     app.config(function ($routeProvider) {
         $routeProvider.when("/",{
@@ -56,6 +58,33 @@
             console.log("Tab clicked");
             this.tab = setTab;
         };
+    });
+
+    app.controller("eventsController", function ($scope) {
+        this.tab = 1;
+
+        this.isSet = function (checkTab) {
+            return (this.tab === checkTab);
+        };
+
+        this.setTab = function (setTab) {
+            this.tab = setTab;
+        };
+
+        $scope.myDate = new Date();
+        $scope.minDate = new Date(
+            $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() - 2,
+            $scope.myDate.getDate());
+        $scope.maxDate = new Date(
+            $scope.myDate.getFullYear(),
+            $scope.myDate.getMonth() + 2,
+            $scope.myDate.getDate());
+        $scope.onlyWeekendsPredicate = function(date) {
+            var day = date.getDay();
+            return day === 0 || day === 6;
+        }
+
     });
 
 })();
